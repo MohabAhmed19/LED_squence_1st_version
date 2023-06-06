60e4de92ee650831c0099581d40b889ea00980df
Mohab <mohabahmedonem8@gmail.com>
2023-06-06 17:07:23 +0300

    MISRA commit

--- LED squence V1.0/MCAL/DIO/DIO.c
+++ LED squence V1.0/MCAL/DIO/DIO.c
@@ -16,34 +16,26 @@ uint8_t DIO_init(uint8_t PORTNumber, uint8_t PinNumber, uint8_t direction)
 		case Port_A:
 			if(direction == in)
 			Clear_Bit(DDRA,PinNumber);
-			else if(direction == out)
-			Set_Bit(DDRA,PinNumber);
 			else
-			error=1;
+			Set_Bit(DDRA,PinNumber);
 			break;
 		case Port_B:
 			if(direction == in)
 			Clear_Bit(DDRB,PinNumber);
-			else if(direction == out)
-			Set_Bit(DDRB,PinNumber);
 			else
-			error=1;
+			Set_Bit(DDRB,PinNumber);
 			break;
 		case Port_C:
 			if(direction == in)
 			Clear_Bit(DDRC,PinNumber);
-			else if(direction == out)
-			Set_Bit(DDRC,PinNumber);
 			else
-			error=1;
+			Set_Bit(DDRC,PinNumber);
 			break;
 		case Port_D:
 			if(direction == in)
 			Clear_Bit(DDRD,PinNumber);
-			else if(direction == out)
-			Set_Bit(DDRD,PinNumber);
 			else
-			error=1;
+			Set_Bit(DDRD,PinNumber);
 			break;
 			default:
 			error=1;
@@ -60,34 +52,26 @@ uint8_t DIO_init(uint8_t PORTNumber, uint8_t PinNumber, uint8_t direction)
 			case Port_A:
 				if(value == low)
 				Clear_Bit(PORTA,PinNumber);
-				else if(value == high)
-				Set_Bit(PORTA,PinNumber);
 				else
-				error=1;
+				Set_Bit(PORTA,PinNumber);
 				break;
 			case Port_B:
 				if(value == low)
 				Clear_Bit(PORTB,PinNumber);
-				else if(value == high)
-				Set_Bit(PORTB,PinNumber);
 				else
-				error=1;
+				Set_Bit(PORTB,PinNumber);
 				break;
 			case Port_C:
 				if(value == low)
 				Clear_Bit(PORTC,PinNumber);
-				else if(value == high)
-				Set_Bit(PORTC,PinNumber);
 				else
-				error=1;
+				Set_Bit(PORTC,PinNumber);
 				break;
 			case Port_D:
 				if(value == low)
 				Clear_Bit(PORTD,PinNumber);
-				else if(value == high)
-				Set_Bit(PORTD,PinNumber);
 				else
-				error=1;
+				Set_Bit(PORTD,PinNumber);
 				break;
 				default:
 				error=1;
